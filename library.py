86a0db1afc6c05df56f24fa8d1bb16766fa309a4
Daniel Wong <drojfjord@gmail.com>
2021-05-25 23:54:43 +1000

    Activate/deactivate internal LED on schedule

--- library.py
+++ library.py
@@ -2,16 +2,26 @@ import busio
 import adafruit_pcf8523
 import time
 import board
+import pwmio
+import neopixel
 
-def rampLightOn():
-    pass
+class LEDDriver:
+    def __init__(self, led):
+        self.led = led
+        self.led.brightness = 1.0
 
-def rampLightOff():
-    pass
+    def rampLightOn(self):
+        self.led[0] = (255, 255, 255)
+
+    def rampLightOff(self):
+        self.led[0] = (0, 0, 0)
+
 
 class Automation:
-    def __init__(self, rtc):
-        self.rtc = rtc
+    def __init__(self, rtc, led):
+        self.rtc = rtc # type: adafruit_pcf8523.PCF8523
+        self.led = led # type: LEDDriver
+        self.wasActive = False
 
     def print_time(self):
         days = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
@@ -26,28 +36,43 @@ class Automation:
         # yearday is not supported, isdst can be set but we don't do anything with it at this time
         t = time.struct_time((year, month, day, hour,  minute,  second, weekday, -1, -1)) # yday, isdst
         print("Setting time to:", t)     # uncomment for debugging
-        rtc.datetime = t
+        self.rtc.datetime = t
 
     def check_actions(self):
-        startHour = 9
-        startMinute = 30
+        startHour = 23
+        startMinute = 00
         startUnits = startHour * 60 + startMinute
-        endHour = 11
-        endMinute = 30
+        endHour = 23
+        endMinute = 1
         endUnits = endHour * 60 + endMinute
 
         t = self.rtc.datetime
-        currentUnit = t.tm_hour * 60 + t.tm_second
+        currentUnit = t.tm_hour * 60 + t.tm_min
 
+        # Current logic will turn on as soon as the hour/minute match start time (at the start of the minute)
+        # And will turn off as soon as hour/minute match end time (at the start of the minute)
         if startUnits < endUnits:
-            active = currentUnit > startUnits and currentUnit < endUnits
+            active = currentUnit >= startUnits and currentUnit < endUnits
         else:
             # This case occurs when the action runs over midnight
-            active = currentUnit > startUnits or currentUnit < endUnits #not (endUnits > currentUnit > startUnits)
+            active = currentUnit >= startUnits or currentUnit < endUnits #not (endUnits > currentUnit > startUnits)
 
-        if not wasActive and active:
-            startPeriodAction
+        print("StartUnit %s EndUnit %s CurrentUnit %s Active %s" % (startUnits, endUnits, currentUnit, active))
 
-        if wasActive and not active:
-            endPeriodAction
+        if not self.wasActive and active:
+            print("Turning lights on")
+            self.led.rampLightOn()
 
+        if self.wasActive and not active:
+            print("Turning lights off")
+            self.led.rampLightOff()
+
+        self.wasActive = active
+
+def getAutomation():
+    myI2C = busio.I2C(board.SCL, board.SDA)
+    rtc = adafruit_pcf8523.PCF8523(myI2C)
+    led = neopixel.NeoPixel(board.NEOPIXEL, 1)
+    ledDriver = LEDDriver(led)
+    automation = Automation(rtc, ledDriver)
+    return automation
--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import busio
 import adafruit_pcf8523
 import time
 import board
-from library import Automation
+import library
+from library import Automation, LEDDriver
 
 def test():
     print("asdf")
 
 if __name__ == '__main__':
-    myI2C = busio.I2C(board.SCL, board.SDA)
-    rtc = adafruit_pcf8523.PCF8523(myI2C)
-    automation = Automation(rtc)
+    automation = library.getAutomation()
 
     while True:
         automation.print_time()
+        automation.check_actions()
         time.sleep(1) # wait a second
